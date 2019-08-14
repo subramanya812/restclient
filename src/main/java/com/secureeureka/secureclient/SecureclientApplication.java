e24641b61650b929847dfc94eee01fa36b035041
subramanya812 <sunil.subramanya812@gmail.com>
2019-08-14 16:31:21 +0530

    change the client url

--- src/main/java/com/secureeureka/secureclient/SecureclientApplication.java
+++ src/main/java/com/secureeureka/secureclient/SecureclientApplication.java
@@ -27,7 +27,7 @@ public class SecureclientApplication {
         //add request header
         con.setRequestProperty("Content-Type", "application/json");
         con.setDoOutput(true);
-        String inputJson = "{ \"instance\": {  \"instanceId\": \"localhost:8097\",\"hostName\": \"localhost\", \"app\": \"java_eureka_client\", \"vipAddress\": \"java_eureka_client\", \"secureVipAddress\": \"java_eureka_client\", \"status\": \"UP\", \"port\": { \"$\": \"8097\", \"@enabled\": \"true\" }, \"securePort\": { \"$\": \"443\", \"@enabled\": \"false\" }, \"healthCheckUrl\": \"http://localhost:8097/health\", \"statusPageUrl\": \"http://localhost:8097/info\", \"homePageUrl\": \"http://localhost:8097\", \"dataCenterInfo\": { \"@class\": \"com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo\", \"name\": \"MyOwn\" } } }";
+        String inputJson = "{ \"instance\": {  \"instanceId\": \"localhost:8097/hello\",\"hostName\": \"localhost\", \"app\": \"java_eureka_client\", \"vipAddress\": \"java_eureka_client\", \"secureVipAddress\": \"java_eureka_client\", \"status\": \"UP\", \"port\": { \"$\": \"8097\", \"@enabled\": \"true\" }, \"securePort\": { \"$\": \"443\", \"@enabled\": \"false\" }, \"healthCheckUrl\": \"http://localhost:8097/health\", \"statusPageUrl\": \"http://localhost:8097/info\", \"homePageUrl\": \"http://localhost:8097\", \"dataCenterInfo\": { \"@class\": \"com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo\", \"name\": \"MyOwn\" } } }";
         OutputStream os = con.getOutputStream();
         os.write(inputJson.getBytes());
         os.flush();
